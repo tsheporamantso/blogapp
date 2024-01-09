15dde5f36c7666ae7e9a0f3cb28348d2c99b8d37
Gladwin Tshepo Ramantso <tshepo.ramantso@outlook.com>
2024-01-09 14:36:18 +0200

    Update db/migrate/20240109113342_create_like.rb
    Co-authored-by: Omar Muhammad <eng.omarmuhammad@gmail.com>

--- db/migrate/20240109113342_create_like.rb
+++ db/migrate/20240109113342_create_like.rb
@@ -1,8 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.references :user, foreign_key: { to_table: :users }
-      t.references :post, foreign_key: { to_table: :posts }
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
