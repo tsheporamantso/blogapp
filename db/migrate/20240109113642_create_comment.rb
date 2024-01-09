f90041e1eeb2613a1f2a59eedf20d2b96c8a35f3
Gladwin Tshepo Ramantso <tshepo.ramantso@outlook.com>
2024-01-09 14:35:39 +0200

    Update db/migrate/20240109113642_create_comment.rb
    Co-authored-by: Omar Muhammad <eng.omarmuhammad@gmail.com>

--- db/migrate/20240109113642_create_comment.rb
+++ db/migrate/20240109113642_create_comment.rb
@@ -2,8 +2,8 @@ class CreateComment < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :user, foreign_key: { to_table: :users }
-      t.references :post, foreign_key: { to_table: :posts }
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
