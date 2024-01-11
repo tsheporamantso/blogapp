e7065822b2e96c0287cc66956c5198cfffa3a9c7
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-11 08:37:35 +0200

    Add rspec test cases for user validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'Gladwin Ramantso' }
+    photo { 'example.jpg' }
+    bio { 'Developer from South Africa' }
+    post_counter { 0 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a name' do
+    user = FactoryBot.build(:user, name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'should only take integer values' do
+    user = FactoryBot.build(:user, post_counter: "a")
+    expect(user).to_not be_valid
+  end
+
+  it 'should not be valid with negative post_counter' do
+    user = FactoryBot.build(:user, post_counter: -1)
+    expect(user).to_not be_valid
+  end
+
+  it 'should be valid with non-negative post_counter' do
+    user = FactoryBot.build(:user, post_counter: 1)
+    expect(user).to be_valid
+  end
 end
