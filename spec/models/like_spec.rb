06cae33f7c61ab2a196c0be614696f44b563bd04
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-11 11:44:41 +0200

    Add spec test case for update likes counter method

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_likes_counter' do
+    it 'updates post likes_counter after save' do
+      user = FactoryBot.create(:user)
+      post = FactoryBot.create(:post, author: user)
+
+      expect(post.likes_counter).to eq(0)
+
+      FactoryBot.create(:like, user:, post:)
+      post.reload
+
+      expect(post.likes_counter).to eq(1)
+    end
+  end
 end
