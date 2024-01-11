8b261c5eb74241f6474ffb25900e84a11deb283f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-11 11:58:17 +0200

    Add test case for method that update comments counter

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'update_comment_counter' do
+    it 'update post comment_counter after save' do
+      user = FactoryBot.create(:user)
+      post = FactoryBot.create(:post, author: user)
+
+      expect(post.comments_counter).to eq(0)
+      
+      FactoryBot.create(:comment, user:, post:)
+      post.reload
+
+      expect(post.comments_counter).to eq(1)
+
+    end
+  end
 end
