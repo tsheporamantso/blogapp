aba1a6439404113c79facda2cfa93c067e4496a7
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-11 11:12:25 +0200

    Add rspec test cases for 5 most recent comment method and author update counter method

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -31,4 +31,25 @@ RSpec.describe Post, type: :model do
       expect(post).to be_valid
     end
   end
+
+  describe '#five_most_recent_comments' do
+    it 'it returns five most recent comments' do
+      post = FactoryBot.create(:post, author: user)
+      FactoryBot.create_list(:comment, 3, post:, created_at: 4.days.ago)
+      recent_comments = FactoryBot.create_list(:comment, 5, post:)
+
+      expect(post.five_most_recent_comments).to eq(recent_comments.reverse)      
+    end
+  end
+  
+  describe '#update_post_counter'do
+    it "updates author's posts_counter after save" do
+      expect(user.post_counter).to eq(0)
+
+      FactoryBot.create(:post, author: user)
+      user.reload
+
+      expect(user.post_counter).to eq(1)
+    end
+  end
 end
