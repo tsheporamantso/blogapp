93a994c7f7faa995947305d6abfdd15aa3523881
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-15 11:08:44 +0200

    Add nested resourceful route for post with index action

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :post, only: [:index]
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
