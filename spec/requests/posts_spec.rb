6575f26398b1681b086948b3348e19b010518871
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-16 13:47:19 +0200

    Correct rubocop offenses

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
     let(:user) { create(:user) }
-    
+
     it 'returns a success response' do
       get user_posts_path(user)
       expect(response).to have_http_status(:success)
     end
-    
-    it "renders the correct template" do
+
+    it 'renders the correct template' do
       get user_posts_path(user)
       expect(response).to render_template(:index)
     end
-    
-    it "includes correct placeholder text" do
+
+    it 'includes correct placeholder text' do
       get user_posts_path(user)
-      expect(response.body).to include("Welcome to posts index page")
+      expect(response.body).to include('Welcome to posts index page')
     end
   end
 
@@ -24,19 +24,19 @@ RSpec.describe 'Posts', type: :request do
     let(:user) { create(:user) }
     let(:post) { create(:post, author: user) }
 
-    it "returns a success response" do
+    it 'returns a success response' do
       get user_post_path(user, post)
       expect(response).to have_http_status(:success)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       get user_post_path(user, post)
       expect(response).to render_template(:show)
     end
 
-    it "includes the correct placeholder text" do
+    it 'includes the correct placeholder text' do
       get user_post_path(user, post)
-      expect(response.body).to include("Welcome to post show page")
+      expect(response.body).to include('Welcome to post show page')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,37 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
-    it "returns a successful response" do
+    it 'returns a successful response' do
       get users_path
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders a correct template" do
+    it 'renders a correct template' do
       get users_path
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text" do
+    it 'includes correct placeholder text' do
       get users_path
-      expect(response.body).to include("Welcome to user index page!")
+      expect(response.body).to include('Welcome to user index page!')
     end
   end
 
   describe 'GET #show' do
     let(:user) { create(:user) }
-    it "returns a successful response" do
+    it 'returns a successful response' do
       get user_path(user)
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders a correct template" do
+    it 'renders a correct template' do
       get user_path(user)
       expect(response).to render_template(:show)
     end
 
-    it "includes the correct placeholder text" do
+    it 'includes the correct placeholder text' do
       get user_path(user)
-      expect(response.body).to include("Welcome to user show page!")
+      expect(response.body).to include('Welcome to user show page!')
     end
   end
 end
