590c0c7c0663c8e1a8dab60df6729847d4897518
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-16 12:53:33 +0200

    Add test cases for user show action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders a correct tamplate" do
+    it "renders a correct template" do
       get users_path
       expect(response).to render_template(:index)
     end
@@ -15,6 +15,24 @@ RSpec.describe 'Users', type: :request do
     it "includes correct placeholder text" do
       get users_path
       expect(response.body).to include("Welcome to user index page!")
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { create(:user) }
+    it "returns a successful response" do
+      get user_path(user)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders a correct template" do
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes the correct placeholder text" do
+      get user_path(user)
+      expect(response.body).to include("Welcome to user show page!")
     end
   end
 end
