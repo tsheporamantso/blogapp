2bf19a57815544177a142e3abb21857e2cf80227
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-16 12:29:33 +0200

    Add users index action test cases

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    it "returns a successful response" do
+      get users_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders a correct tamplate" do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it "includes correct placeholder text" do
+      get users_path
+      expect(response.body).to include("Welcome to user index page!")
+    end
   end
 end
