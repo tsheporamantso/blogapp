6ea2714fa36c7a8e878481c747699d16a4964174
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-16 13:20:24 +0200

    Add test cases for Post index action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+    it 'returns a success response' do
+      get user_posts_path(user)
       expect(response).to have_http_status(:success)
+    end
+    it "renders the correct template" do
+      get user_posts_path(user)
+      expect(response).to render_template(:index)
+    end
+    it "includes correct placeholder text" do
+      get user_posts_path(user)
+      expect(response.body).to include("Welcome to posts index page")
     end
   end
 end
