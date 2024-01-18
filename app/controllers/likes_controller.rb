bcbe59b5a2709653fef9866a7921d1cf758a0b5b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-18 19:59:03 +0200

    Define private set_user_and_post action that returns user and post by id

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,17 @@
 class LikesController < ApplicationController
+  before_action :set_user_and_post, only: [:new, :create]
+  
   def new
+    @like = Like.new
   end
 
   def create
+  end
+
+  private
+
+  def set_user_and_post
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
   end
 end
