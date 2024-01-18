a791d36ede2e8d63144734a4eabe9d691c28888d
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-18 18:34:45 +0200

    Define new and create actions for comments controller and also add strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params.merge(user_id: current_user.id))
+
+    if @comment.save
+      redirect_back_or_default(user_post_comments_path(@user, @post))
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
