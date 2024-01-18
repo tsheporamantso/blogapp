f129e05f7eb147f03dfd1f589bafbacfa74c324d
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-18 20:06:58 +0200

    Define create action that instantiate post and like instance variables if like variable is save than redirect to post show page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,19 @@
 class LikesController < ApplicationController
   before_action :set_user_and_post, only: [:new, :create]
-  
+
   def new
     @like = Like.new
   end
 
   def create
+    @post = @user.posts.find(params[:post_id])
+    @like = @post.likes.new(user: current_user)
+
+    if @like.save
+      redirect_to user_post_path(@user, @post), notice: "Like was successfully created."
+    else
+      render :new 
+    end
   end
 
   private
