b0098954cb836f0dff3264dca110fe0f64148153
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-22 20:29:10 +0200

    Add post show spec file with show page test cases

---
+++ post_show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Post show page' do
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post, author: user) }
+  let(:comments) { FactoryBot.create_list(:comment, 5, author: user, post:) }
+
+  before do
+    post.reload
+
+    FactoryBot.create_list(:comment, 5, post:)
+
+    visit user_post_path(user, post)
+  end
+
+  describe 'Post show page' do
+    it 'displays post title' do
+      expect(page).to have_content(post.title)
+    end
+    
+    it 
+  end
+end
