5daeb5dc21c3f069d0abdeebcc71a4bef51af6cf
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-24 12:48:20 +0200

    Add users index action as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
+  root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
