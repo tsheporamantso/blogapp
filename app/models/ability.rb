4dcec282d3083de6fe832ef5ae7849bfa62c4675
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-24 15:48:56 +0200

    Correct typo

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
-      can %i[destroy create], Post, author_id: user_id
+      can %i[destroy create], Post, author_id: user.id
     end
   end
 end
