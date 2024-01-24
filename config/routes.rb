51ec93c9794d63cc2b05d8e5f9c675470fab00fe
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-24 14:16:49 +0200

    Add destroy action on post resourceful route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:new, :create]
     end
