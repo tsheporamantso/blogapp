89caffb61b59ea77286710c7fa061d2475a649aa
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-25 14:42:29 +0200

    Add API folder for post and users controller and render json

---
+++ app/controllers/api/post_controller.rb
@@ -0,0 +1,19 @@
+class Api::PostsController < ApplicationController
+  def comments
+    @post = Post.find(params[:id])
+    render json: { comments: @post.comments }
+  end
+
+  def add_comment
+    @post = Post.find(params[:id])
+    comment_text = params[:comment][:text]
+    @comment = @post.comments.build(text: comment_text)
+    @comment.author = current_user
+
+    if @comment.save
+      render json: { message: 'Comment added successfully' }, status: :created
+    else
+      render json: { error: 'Failed to add comment' }, status: :unprocessable_entity
+    end
+  end
+end
